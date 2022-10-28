c549f6d055ac89648473e828bf92e6b3bcb27d6a
anny <puchkovdd@gmail.com>
2022-10-28 12:29:00 +0300

    complited initial settings

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import express from "express";
-const PORT = 5000;
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 const app = express();
+const DB_URL = process.env.DB_URL;
+app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+const startApp = async () => {
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startApp()
